7bb2897fc2ce9233fea7406d9d278e98dd905ca9
Sidhh Rana <sidhhrana@Sidhhs-MacBook-Air.local>
2026-08-15 00:47:46 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-const CACHE_NAME = 'pufftrack-cache-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'pufftrack-cache-v2';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
@@ -18,7 +8,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys.map((key) => caches.delete(key))
       );
     })
   );
@@ -29,7 +19,7 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request).catch(() => {
-      return caches.match(event.request).then((res) => res || caches.match('/'));
+      return caches.match(event.request);
     })
   );
 });
